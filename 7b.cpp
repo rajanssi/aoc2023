4d01216d4266403e67151c220235922465d09d08
Anssi Rajala <ajrajal4@gmail.com>
2023-12-15 21:15:09 +0200

    day 7

---
+++ 7b.cpp
@@ -0,0 +1,100 @@
+#
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int getValue(char card) {
+  if (card == 'A')
+    return 14;
+  if (card == 'K')
+    return 13;
+  if (card == 'Q')
+    return 12;
+  if (card == 'J')
+    return 1;
+  if (card == 'T')
+    return 10;
+  return card - '0';
+}
+
+int getHandType(const std::string &hand) {
+  std::map<char, int> counts;
+  int jacks = 0;
+  for (char card : hand) {
+    if (card == 'J') jacks++;
+    else counts[card]++;
+  }
+
+  if (jacks == 5) return 7;
+  int n = counts.size();
+  if (n == 1)
+    return 7;
+  if (n == 2) {
+    if (jacks >= 2) return 6;
+    for (auto &p : counts) {
+      if (p.second + jacks == 4 || p.second == 4 || p.second == 1)
+        return 6;
+    return 5;
+    }
+  }
+  if (n == 3) {
+    if (jacks >= 1) return 4;
+    for (auto &p : counts) {
+      if (p.second == 3)
+        return 4;
+    }
+    return 3;
+  }
+  if (n == 4)
+    return 2;
+  return 1;
+}
+
+bool compareHands(const std::pair<std::string, int> &a,
+                  const std::pair<std::string, int> &b) {
+  int typeA = getHandType(a.first);
+  int typeB = getHandType(b.first);
+
+  if (typeA != typeB)
+    return typeA > typeB;
+
+  for (size_t i = 0; i < a.first.length(); ++i) {
+    int valA = getValue(a.first[i]);
+    int valB = getValue(b.first[i]);
+    if (valA != valB)
+      return valA > valB;
+  }
+  return false;
+}
+
+int main() {
+  std::ifstream file("input");
+  std::vector<std::pair<std::string, int>> hands;
+
+  std::string line;
+  while (std::getline(file, line)) {
+    std::istringstream iss(line);
+    std::string cards;
+    int bet;
+
+    iss >> cards;
+    iss >> bet;
+
+    hands.push_back({cards, bet});
+  }
+
+  std::sort(hands.begin(), hands.end(), compareHands);
+
+  int totalWinnings = 0;
+  for (size_t i = 0; i < hands.size(); i++) {
+    totalWinnings += hands[i].second * (hands.size() - i);
+  }
+
+  std::cout << "Total winnings: " << totalWinnings << std::endl;
+  return 0;
+}
+
